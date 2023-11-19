71a2137e293c12d12fe0b242354d447f0bdc1345
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2023-11-19 11:57:08 +0300

    Chore: Update `drizzle-kit` & `drizzle-orm`

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
-import { type Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
 import { env } from "~/env.mjs";
 
-export default {
+export default defineConfig({
   schema: "./src/server/db/schema",
   driver: "mysql2",
   dbCredentials: {
-    connectionString: env.DATABASE_URL,
+    uri: env.DATABASE_URL,
   },
   tablesFilter: ["soko_*"],
-} satisfies Config;
+});
