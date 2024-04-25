0a5c298fb251f32d8e25bb1db35308b806d2340a
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2024-04-25 07:04:47 +0300

    Rfctr: `getBaseUrl` function in `utils.ts`

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,11 +5,11 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const getBaseUrl = () => {
-  if (typeof window !== "undefined") return ""; // Browser should use relative url
+export function getBaseUrl() {
+  if (typeof window !== "undefined") return window.location.origin; // Browser should use relative url
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use Vercel url
   return `http://localhost:${process.env.PORT ?? 3000}`; // Dev SSR should use localhost
-};
+}
 
 export const formatter = new Intl.NumberFormat("en-US", {
   style: "currency",
