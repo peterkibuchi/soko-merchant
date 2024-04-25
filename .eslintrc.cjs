37326d50b78581f2361f0d9a11cffd2b021ec5b3
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2024-04-25 07:34:33 +0300

    Feat: Configure Linting for Drizzle

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,18 +2,15 @@
 const config = {
   parser: "@typescript-eslint/parser",
   parserOptions: { project: true },
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "drizzle"],
   extends: [
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
   ],
   rules: {
-    // These opinionated rules are enabled in stylistic-type-checked above.
-    // Feel free to reconfigure them to your own preference.
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
-
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       { prefer: "type-imports", fixStyle: "inline-type-imports" },
@@ -23,6 +20,14 @@ const config = {
     "@typescript-eslint/no-misused-promises": [
       "error",
       { checksVoidReturn: { attributes: false } },
+    ],
+    "drizzle/enforce-delete-with-where": [
+      "error",
+      { drizzleObjectName: ["db"] },
+    ],
+    "drizzle/enforce-update-with-where": [
+      "error",
+      { drizzleObjectName: ["db"] },
     ],
   },
 };
